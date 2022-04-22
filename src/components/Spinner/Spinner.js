8c69724fbb8425077f6a5c72d7cef92ca4438221
tamerlan <remat01@mail.ru>
2022-04-22 11:15:17 +0600

    feat: added spinner

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import "./Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-dual-ring-xiqqm4odo9n">
+      <div class="ldio-ntp08tchtq">
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Spinner };
